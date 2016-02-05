04505a6064f192c1a648187bb8454ea9bd126a88
Michael <thatisnotthecase@gmail.com>
2016-02-05 01:58:02 +0000

    dirs

---
+++ Makefile
@@ -0,0 +1,15 @@
+dirs:
+	mkdir -pv \
+	application/layouts/scripts/ \
+	application/forms/Twitter/ \
+	application/forms/FormHelper/ \
+	application/views/helpers/ \
+	application/views/scripts/ \
+	application/views/scripts/index \
+	application/views/scripts/logs \
+	application/views/scripts/admin \
+	application/views/scripts/auth \
+	application/views/scripts/sites \
+	application/views/scripts/maps \
+	application/views/scripts/error
+ 
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-setlocale(LC_ALL, 'ru_RU.CP1251');
-define('APPLICATION_PATH', __DIR__ . '/application');
-define('APPLICATION_ENV', 'production');
-set_include_path(implode(PATH_SEPARATOR, array(APPLICATION_PATH . '/library', get_include_path(),)));
-require_once 'Zend/Application.php';
+require '.ZEND_PREAMBLE.index.include';
 $application = new Zend_Application( APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
 $appBootstrap = $application->bootstrap();
 $appBootstrap ->run();
