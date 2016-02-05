d9124b6778318c1ad5d0f2167ba05321d2593350
Michael <thatisnotthecase@gmail.com>
2016-02-05 03:17:40 +0000

    dirs

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,1 +1,16 @@
-<?php class IndexController extends App_Controller {}
+<?php
+
+class IndexController extends App_Controller {
+
+    public function init() {
+        $this->view->styles = $this->styles;
+        $this->view->scripts = $this->scripts;
+    }
+
+    public function indexAction() {
+        // $this->_redirect('/sites/');
+        // вывод загаловков
+        $this->view->title = "Авторизация";
+        $this->view->headTitle($this->view->title, 'PREPEND');
+    }
+}
